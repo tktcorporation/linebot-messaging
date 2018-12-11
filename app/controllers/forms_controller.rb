56d607929511bb56875b3a324ff087627b124555
tkt <tktcorporation.go@gmail.com>
2018-12-11 19:20:22 +0900

    bot_layoutを使用しているところでbot名を表示するように

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -3,14 +3,15 @@ class FormsController < ApplicationController
   layout 'bot_layout'
 
   def index
-    bot = Bot.get(params[:bot_id])
-    @forms = bot.forms.includes(:converted_lineusers, :session_lineusers).where(is_active: false)
-    @active_form = bot.forms.includes(:converted_lineusers, :session_lineusers).find_by(is_active: true)
+    @bot = Bot.get(params[:bot_id])
+    @forms = @bot.forms.includes(:converted_lineusers, :session_lineusers).where(is_active: false)
+    @active_form = @bot.forms.includes(:converted_lineusers, :session_lineusers).find_by(is_active: true)
     @new_form = Form.new
   end
 
   def show
     @form = Form.includes(:quick_replies).get(params[:id])
+    @bot = @form.bot
     @quick_replies = @form.quick_replies.includes(:quick_reply_items)
     @quick_reply = @form.quick_replies.new
   end
