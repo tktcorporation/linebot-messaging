ca080e3884cde6bf59108900e3ad88a030fb0706
tkt <tktcorporation.go@gmail.com>
2018-12-11 18:38:48 +0900

    freetextとscheduleのクイックリプライ 作成を可能に(Controller,Model)

--- app/models/quick_reply_schedule.rb
+++ app/models/quick_reply_schedule.rb
@@ -3,4 +3,9 @@ class QuickReplySchedule < ApplicationRecord
 
   validates :quick_reply_id, presence: true
   validates :duration_days, presence: true
+
+  def self.create_option(quick_reply, duration_days, summary)
+    quick_reply_schedule = QuickReplySchedule.find_or_initialize_by(quick_reply_id: quick_reply.id)
+    quick_reply_schedule.update_attributes!(duration_days: duration_days, summary: summary)
+  end
 end
