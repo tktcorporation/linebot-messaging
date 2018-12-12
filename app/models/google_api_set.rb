d6a96033191ca53f0165e320bedb349be8e13f9a
tkt <tktcorporation.go@gmail.com>
2018-12-12 17:01:31 +0900

    google_apiを設定可能に

--- app/models/google_api_set.rb
+++ app/models/google_api_set.rb
@@ -1,7 +1,13 @@
 class GoogleApiSet < ApplicationRecord
   belongs_to :bot
 
-  validates :access_token, presence: true
+  validates :client_secret, presence: true
+  validates :client_id, presence: true
   validates :bot_id, presence: true
+
+  def self.update_or_create(bot_id, client_id, client_secret)
+    google_api_set = self.find_or_initialize_by(bot_id: bot_id)
+    google_api_set.update_attributes!(client_id: client_id, client_secret: client_secret)
+  end
 
 end
