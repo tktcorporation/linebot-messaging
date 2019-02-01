9c3792c864ac9768895ff4e21fd0b1a474ca40aa
tkt <tktcorporation.go@gmail.com>
2019-02-01 18:09:31 +0900

    ステータス変更ができないバグを修正

--- app/models/bot/lineuser_status.rb
+++ app/models/bot/lineuser_status.rb
@@ -5,7 +5,7 @@ class Bot::LineuserStatus < ApplicationRecord
   validates :status_id, numericality: true, allow_blank: true
 
   def self.create_or_update(lineuser_status_params, lineuser)
-    if lineuser.status.present?
+    if lineuser.lineuser_status.present?
       lineuser.lineuser_status.update_attributes!(lineuser_status_params)
     else
       status = lineuser.build_lineuser_status(lineuser_status_params)
