a107af6b5534c43938c0777279f2eeaf36b0db2c
tkt <tktcorporation.go@gmail.com>
2019-02-15 22:29:26 +0900

    lineuserにtextが入ってバグが出ていた問題を修正

--- db/migrate/20190215084105_create_reply_actions.rb
+++ db/migrate/20190215084105_create_reply_actions.rb
@@ -1,6 +1,6 @@
 class CreateReplyActions < ActiveRecord::Migration[5.2]
   def change
-    create_table :bot_reply_actions do |t|
+    create_table :bot_reply_actions, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
       t.integer :quick_reply_id, foreign_key: true
       t.integer :bot_id, foreign_key: true
       t.string :name, null: false
