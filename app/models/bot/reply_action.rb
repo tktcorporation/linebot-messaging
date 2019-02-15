78cc33a15e4e5c40c87ce1fe26ac7404f9723d4a
tkt <tktcorporation.go@gmail.com>
2019-02-15 22:01:09 +0900

    文言に反応した自動応答を可能に

---
+++ app/models/bot/reply_action.rb
@@ -0,0 +1,12 @@
+class Bot::ReplyAction < ApplicationRecord
+  belongs_to :bot
+  belongs_to :quick_reply
+
+  scope :is_active, ->{ where(is_active: true) }
+
+  validates :name, presence: true, lt4bytes: true, length: { in: 1..250 }
+  validates :quick_reply_id, numericality: true
+  validates :text, presence: true, lt4bytes: true
+  validates :is_active, presence: true
+
+end
---
+++ db/migrate/20190215084105_create_reply_actions.rb
@@ -0,0 +1,13 @@
+class CreateReplyActions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bot_reply_actions do |t|
+      t.integer :quick_reply_id, foreign_key: true
+      t.integer :bot_id, foreign_key: true
+      t.string :name, null: false
+      t.string :text, null: false
+      t.boolean :is_active, default: 1
+
+      t.timestamps
+    end
+  end
+end
