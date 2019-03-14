27724de95fe1cad3ed2fd845b757b32ecdc8d838
tkt <tktcorporation.go@gmail.com>
2019-03-15 07:36:40 +0900

    バリデーションの設定

--- app/models/lineuser/invitation_code.rb
+++ app/models/lineuser/invitation_code.rb
@@ -1,6 +1,9 @@
 class Lineuser::InvitationCode < ApplicationRecord
   belongs_to :lineuser
 
+  validates :code, presence: true
+  validates :lineuser_id, presence: true
+
   def self.get(id)
     self.find(id)
   end
