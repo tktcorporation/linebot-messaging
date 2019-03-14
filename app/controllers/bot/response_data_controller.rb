6da43e808e23c274339c1cae6868e89920b93259
tkt <tktcorporation.go@gmail.com>
2019-03-14 14:58:52 +0900

    n+1問題の解消

--- app/controllers/bot/response_data_controller.rb
+++ app/controllers/bot/response_data_controller.rb
@@ -3,7 +3,7 @@ class Bot::ResponseDataController < ApplicationController
   def index
     @bot = Bot.get(params[:bot_id])
     @quick_reply_list = @bot.quick_replies.where(is_normal_message: false)
-    @lineusers = @bot.lineusers.includes(:response_data)
+    @lineusers = @bot.lineusers.includes(:response_data, :converted_lineuser, :quick_reply, :lineuser_status)
     @status_array = @bot.get_status_array
   end
 end
