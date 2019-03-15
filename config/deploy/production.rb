c2cdbc23215e53ebe5f985c6931050b757a34403
tkt <tktcorporation.go@gmail.com>
2019-03-15 23:21:27 +0900

    デプロイ用の設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -21,6 +21,24 @@
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
+# set :branch, 'develop'
+
+# server '13.113.141.0', user: 'centos', roles: %w(app web db)
+
+# set :ssh_options, {
+#   keys: %w(~/.ssh/***REMOVED***),
+#   forward_agent: true,
+#   auth_methods: %w(publickey)
+# }
+
+server '54.249.33.138', user: '***REMOVED***', roles: %w(app web db)
+
+set :ssh_options, {
+  keys: %w(***REMOVED***),
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
+
 
 
 # Configuration
