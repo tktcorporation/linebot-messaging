dc445960e4608dbaa49a92e9b09cbecc389357a0
tkt <tktcorporation.go@gmail.com>
2019-03-16 20:51:58 +0900

    不要な処理を削除

--- app/controllers/google_auth_controller.rb
+++ app/controllers/google_auth_controller.rb
@@ -20,23 +20,6 @@ class GoogleAuthController < ApplicationController
     redirect_to bot_url(id: bot.id)
   end
 
-  def create_event
-    GoogleCalendar.create_event(QuickReply.find(37), Time.now, 1, Lineuser.find(13))
-    redirect_to "/"
-  end
-
-  def test_create
-    bot = Bot.get(params[:bot_id])
-    GoogleCalendar.test_create_event(bot, Time.now, 1)
-    redirect_back(fallback_location: root_path)
-  end
-
-  def get_events
-    day = Time.now
-    Manager.extract_free_time(Manager.available_array_week(day), day)
-    redirect_to "/"
-  end
-
   private
   def client_options(bot)
     return client_option = {
