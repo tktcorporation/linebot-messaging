02704704cccb9049a89bf8480052cbd135825ea9
tkt <tktcorporation.go@gmail.com>
2019-03-19 06:22:25 +0900

    table名が適切でなかったため変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_172808) do
+ActiveRecord::Schema.define(version: 2019_03_18_211852) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -89,6 +89,13 @@ ActiveRecord::Schema.define(version: 2019_03_18_172808) do
     t.integer "bot_id", null: false
     t.string "name", limit: 50, default: "", null: false
     t.boolean "deleted", null: false
+  end
+
+  create_table "bot_stock_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "bot_id"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "bots", id: :integer, unsigned: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -268,13 +275,6 @@ ActiveRecord::Schema.define(version: 2019_03_18_172808) do
   create_table "session_lineusers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "form_id"
     t.integer "lineuser_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "stock_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "bot_id"
-    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
