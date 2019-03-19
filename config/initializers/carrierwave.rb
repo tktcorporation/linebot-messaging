98873e2fb20421526aefb50f44e3f7fb11b848d7
tkt <tktcorporation.go@gmail.com>
2019-03-19 23:45:02 +0900

    hostとpublic設定を追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,6 +7,8 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = '***REMOVED***'
+    config.asset_host = 'https://s3.amazonaws.com/***REMOVED***'
+    config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
