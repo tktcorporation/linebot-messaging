872ff169df17ffc7b1047bd30427b41ceaa40b99
tkt <tktcorporation.go@gmail.com>
2019-03-20 02:18:56 +0900

    s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = '***REMOVED***'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/***REMOVED***'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com'
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
