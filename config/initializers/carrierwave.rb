9d2d07a1a11950e68cbe15cc94b73734c99112e3
tkt <tktcorporation.go@gmail.com>
2019-03-20 02:29:18 +0900

    初心に帰る

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,15 +7,12 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'catalist-s3-001'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com'
-    config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id) + "sasasasa",
+      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
       aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
       region: "ap-northeast-1",
       path_style: true
-      #endpoint: Rails.application.credentials.dig(:aws, :endpoint)
     }
   else
     config.storage :file
