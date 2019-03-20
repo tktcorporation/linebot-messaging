6c57d9c954eae9a50f1a352a035bddba074b077a
tkt <tktcorporation.go@gmail.com>
2019-03-20 13:51:51 +0900

    hostを設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,6 +7,7 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = '***REMOVED***'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/***REMOVED***'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
