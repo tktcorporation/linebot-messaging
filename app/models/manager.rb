e57c2d2fb311f876d72ff5059b901ddd8d60880a
tkt <tktcorporation.go@gmail.com>
2019-04-08 13:27:19 +0900

    putsだとlogファイルには出力されないらしい

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -439,12 +439,12 @@ class  Manager
     if text.present?
       message = self.save_message(event, type, text)
     end
-    p "======================"
-    p "更新前"
-    p lastmessage_id
-    p "更新後"
-    p lineuser.lastmessage_id
-    p "======================"
+    logger.debug "======================"
+    logger.debug "更新前"
+    logger.debug lastmessage_id
+    logger.debug "更新後"
+    logger.debug lineuser.lastmessage_id
+    logger.debug "======================"
     if message && lastmessage_id == lineuser.lastmessage_id
       lineuser.update_lastmessage(message)
     end
