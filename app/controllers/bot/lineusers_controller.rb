ba88e26060400b984a2fc4599316b9a5050462d9
tkt <tktcorporation.go@gmail.com>
2019-04-19 23:09:21 +0900

    検索件数を制限

--- app/controllers/bot/lineusers_controller.rb
+++ app/controllers/bot/lineusers_controller.rb
@@ -1,27 +1,9 @@
 class Bot::LineusersController < ApplicationController
-  before_action :check_auth
   layout 'bot_layout'
-
   def index
-    @bot = current_user.bots.includes(:lineusers, :reminds).includes(:lineusers => :messages).get(params[:bot_id])
-    @newremind = Remind.new
+    @bot = current_user.bots.get(params[:bot_id])
+    @quick_reply_list = @bot.quick_replies.where(is_normal_message: false)
+    @lineusers = @bot.lineusers.includes(:response_data, :converted_lineuser, :quick_reply, :lineuser_status, :lastmessage).followed.order("messages.created_at desc").limit(100)
+    @status_array = @bot.get_status_array
   end
-
-  def show
-    form = Form.includes(:quick_replies).get(params[:id])
-    @bot = form.bot
-    @quick_replies = form.quick_replies.includes(:quick_reply_items)
-    @quick_reply = form.quick_replies.new
-  end
-
-  private
-    def check_auth
-      return if !params[:id]
-      if Lineuser.get(params[:id]).bot.user_id != @current_user.id
-        render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
-      end
-    end
-    def form_params
-      params.require(:lineuser).permit(:name)
-    end
 end
