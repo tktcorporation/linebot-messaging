6ae7e9b9d175a4b934d44dc031f9b0f4e3f84318
tkt <tktcorporation.go@gmail.com>
2019-04-28 07:49:46 +0900

    messageの保存場所を集約

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,11 +8,41 @@ class Message < ApplicationRecord
 
   scope :undeleted, ->{ where(deleted: false) }
   scope :order_asc, ->{ order(created_at: :asc) }
+
   def self.get_plural_with_lineuser_id(lineuser_id)
     Message.where(lineuser_id: lineuser_id).order_asc
   end
+
   def self.get(id)
     self.find(id)
+  end
+
+  def self.create_quick_reply_message(lineuser, quick_reply)
+    self.new(content: "クイックリプライ：" + quick_reply.name, lineuser_id: lineuser.id, to_bot: false, msg_type: 0)
+    save!
+    self
+  end
+
+  def self.create_message(lineuser, to_bot, content)
+    self.new(lineuser_id: lineuser.id, to_bot: false, content: content, msg_type: 0)
+    save!
+    self
+  end
+
+  def self.create_image(lineuser, to_bot, stock_image)
+    self.new(lineuser_id: lineuser.id, to_bot: false, content: "[画像: #{stock_image.id}::#{stock_image.image.url}]", msg_type: 1)
+    save!
+    self
+  end
+
+  def self.create_with_type(lineuser, to_bot, type, content)
+    if !(type.class === Integer)
+      type = 0
+      Rails.logger.fatal "msg_type is not Integer. [Message.create_with_type]"
+    end
+    self.new(lineuser_id: lineuser.id, to_bot: true, msg_type: type, content: text)
+    save!
+    self
   end
 
   # def self.bulk_insert(lineusers, text, to_bot)
