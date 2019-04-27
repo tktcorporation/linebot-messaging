8405ad0dce2126ca45e6beb60e2db4b0e1f5cfd4
tkt <tktcorporation.go@gmail.com>
2019-04-27 23:54:59 +0900

    CV時かつstatusが存在していない時

--- app/models/lineuser.rb
+++ app/models/lineuser.rb
@@ -72,7 +72,9 @@ class Lineuser < ApplicationRecord
     # sessionとconvertを一致させる
     converted_lineuser.form_id = session_lineuser.form_id
     converted_lineuser.save!
-    self.status = self.bot.statuses.get_cv_status
+    if !self.status.present?
+      self.status = self.bot.statuses.get_cv_status
+    end
     Manager.push_slack_lineuser_data(self)
   end
 
