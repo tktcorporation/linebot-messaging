9718eb0ece2ec0e7e9c859894ae4539041ad46db
tkt <tktcorporation.go@gmail.com>
2019-05-23 01:55:34 +0900

    no message

--- app/controllers/bot/lineusers_controller.rb
+++ app/controllers/bot/lineusers_controller.rb
@@ -3,7 +3,7 @@ class Bot::LineusersController < ApplicationController
   def index
     @bot = current_user.bots.get(params[:bot_id])
     # @quick_reply_list = @bot.quick_replies.where(is_normal_message: false)
-    @lineusers = bot.lineusers.get_open_users
+    @lineusers = @bot.lineusers.get_open_users
     @lineusers = @lineusers.includes(:lastmessage, :quick_reply, :converted_lineuser, :lineuser_status).order("messages.created_at desc").limit(30)
     @status_array = @bot.get_status_array
   end
