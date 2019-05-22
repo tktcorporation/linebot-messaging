8e65f09a2304e53904081e554014b37de0f92cff
tkt <tktcorporation.go@gmail.com>
2019-05-22 23:48:36 +0900

    順番変更

--- app/controllers/bot/lineusers_controller.rb
+++ app/controllers/bot/lineusers_controller.rb
@@ -3,7 +3,7 @@ class Bot::LineusersController < ApplicationController
   def index
     @bot = current_user.bots.get(params[:bot_id])
     # @quick_reply_list = @bot.quick_replies.where(is_normal_message: false)
-    @lineusers = @bot.lineusers.get_open_users.includes(:converted_lineuser, :lineuser_status, :lastmessage).followed.order("messages.created_at desc").limit(50)
+    @lineusers = @bot.lineusers.includes(:converted_lineuser, :lineuser_status, :lastmessage).get_open_users.order("messages.created_at desc").limit(50)
     @status_array = @bot.get_status_array
   end
 end
